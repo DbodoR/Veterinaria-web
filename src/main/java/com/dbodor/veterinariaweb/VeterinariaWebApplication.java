7a5f95dd53077a06cd23c80d7b685dd2fa9a1997
Juan Diego Cano <70496065+DbodoR@users.noreply.github.com>
2026-09-16 18:59:07 -0500

    Creación proyecto Veterinaria web

---
+++ src/main/java/com/dbodor/veterinariaweb/VeterinariaWebApplication.java
@@ -0,0 +1,13 @@
+package com.dbodor.veterinariaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinariaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dbodor/veterinariaweb/VeterinariaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbodor.veterinariaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
